83fccdcdf4076739c58c3403c2fa43ace72f0429
gotokoki1 <72792963+gotokoki1@users.noreply.github.com>
2020-11-05 16:53:06 +0900

    ActiveHash導入バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+  belongs_to :condition_id
+  belongs_to :postage_id
+  belongs_to :prefecture_id
+  belongs_to :prepareday_id
   belongs_to :user
   has_one_attached :image
+
+  validates :category_id, :condition_id, :postage, :prefecture_id, :prepare_day_id, presence: true
+  validates :category_id, :condition_id, :postage_id, :prefecture_id, :prepare_day_id, numericality: { other_than: 1 } 
 end
---
+++ spec/factories/prepare_days.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :prepare_day do
+    
+  end
+end
