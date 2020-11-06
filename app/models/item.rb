86f6a4243bc9ab201344d8fd4a2e153496acbe52
gotokoki1 <72792963+gotokoki1@users.noreply.github.com>
2020-11-06 15:43:52 +0900

    itemモデル単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :category_id, :condition_id, :postage, :prefecture_id, :prepare_day_id, presence: true
+  validates :category_id, :condition_id, :postage, :prefecture_id, :prepare_day_id, :name, :explanation, :image, presence: true
   validates :category_id, :condition_id, :postage_id, :prefecture_id, :prepare_day_id, numericality: { other_than: 1 } 
+  
+  VALID_PRICEL_HALF =                 /\A[0-9]+\z/
+  validates  :price,  presence: true, format: {with: VALID_PRICEL_HALF},length: {minimum: 3, maxinum: 7},numericality: { only_integer: true,
+  greater_than: 300, less_than: 9999999}
+
 end
