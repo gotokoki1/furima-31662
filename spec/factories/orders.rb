45df4acbacdf3c4796bd7c0ab1bee9612c53c009
gotokoki1 <72792963+gotokoki1@users.noreply.github.com>
2020-11-09 12:16:51 +0900

    単体テストコード

---
+++ spec/factories/orders.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order do
+    post_code { 418-0022 }
+    prefecture_id { 2 }
+    city { "富士宮市" }
+    house_number { "小泉566-18" }
+    building_name { "フラット" }
+    phone_number { "08043020930" }
+    token { "tok_abcdefghijk00000000000000000" }
+  end
+end
